22502f7a153467fe099fcb1fbe9e110c16382c09
Arnaud Dollois <arnaud@dollois.com>
2018-08-31 09:50:49 +0200

    Show good and bad answers

--- app/src/main/java/com/dollois/topquizz/controller/GameActivity.java
+++ app/src/main/java/com/dollois/topquizz/controller/GameActivity.java
@@ -3,6 +3,9 @@ package com.dollois.topquizz.controller;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -45,32 +48,31 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         mLevel = intent.getIntExtra("level", 5);
         mFind = intent.getIntExtra("find", 1);
 
-        mQuestionText   = (TextView) findViewById(R.id.activity_game_question_text);
-        mAnswer1Button  = (Button) findViewById(R.id.activity_game_answer1_btn);
-        mAnswer2Button  = (Button) findViewById(R.id.activity_game_answer2_btn);
-        mAnswer3Button  = (Button) findViewById(R.id.activity_game_answer3_btn);
-        mAnswer4Button  = (Button) findViewById(R.id.activity_game_answer4_btn);
-
+        mQuestionText = (TextView) findViewById(R.id.activity_game_question_text);
         mGoodAnswerText = (TextView) findViewById(R.id.activity_game_good_answer_text);
-        mBadAnswerText  = (TextView) findViewById(R.id.activity_game_bad_answer_text);
+        mBadAnswerText = (TextView) findViewById(R.id.activity_game_bad_answer_text);
+
+        mAnswer1Button = (Button) findViewById(R.id.activity_game_answer1_btn);
+        mAnswer2Button = (Button) findViewById(R.id.activity_game_answer2_btn);
+        mAnswer3Button = (Button) findViewById(R.id.activity_game_answer3_btn);
+        mAnswer4Button = (Button) findViewById(R.id.activity_game_answer4_btn);
+
         String mLevelTxt = " joker";
-        if( mLevel > 1)
+        if (mLevel > 1)
             mLevelTxt += "s";
 
-        mBadAnswerText.setText(mLevel+mLevelTxt);
+        mBadAnswerText.setText(mLevel + mLevelTxt);
 
         mAnswer1Button.setOnClickListener(this);
         mAnswer2Button.setOnClickListener(this);
         mAnswer3Button.setOnClickListener(this);
         mAnswer4Button.setOnClickListener(this);
 
+        try {
+            mlistQuestion = new QuestionBank(this.getAssets().open("liste.json"), mFind);
+            this.displayQuestion(mlistQuestion, mGoodAnswer + mBadAnswer);
 
-
-        try{
-            mlistQuestion = new QuestionBank( this.getAssets().open("liste.json"), mFind);
-            this.displayQuestion(mlistQuestion, mGoodAnswer+mBadAnswer);
-
-        }catch (IOException ex) {
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
 
@@ -79,7 +81,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
     private void displayQuestion(QuestionBank listQuestion, int index) {
         Intent intent = getIntent();
-        if(mBadAnswer < mLevel && index < listQuestion.getQuestionList().length) {
+        if (mBadAnswer < mLevel && index < listQuestion.getQuestionList().length) {
             Question currentQuestion = new Question();
             currentQuestion = listQuestion.getQuestionList()[index];
 
@@ -97,15 +99,17 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             mAnswer4Button.setText(currentQuestion.getResponsesList()[3]);
             mAnswer4Button.setTag(currentQuestion.getResponsesList()[3]);
 
+            this.changeColor(Color.WHITE);
+
             mCurrentAnswer = currentQuestion.getAnswer();
-        }else{
+        } else {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
             String textMessage = "";
             User user = intent.getParcelableExtra("user");
-            textMessage = "Bravo " + user.getPseudo() +"!!!"+"\n"+"Voici votre bilan"+"\n" + "Nombre de questions: " + (mGoodAnswer+mBadAnswer);
-            textMessage += "\n"+"Nombre de bonnes réponses: "+ mGoodAnswer;
-            textMessage += "\n"+"Nombre de mauvaise réponses: "+ mBadAnswer;
+            textMessage = "Bravo " + user.getPseudo() + "!!!" + "\n" + "Voici votre bilan" + "\n" + "Nombre de questions: " + (mGoodAnswer + mBadAnswer);
+            textMessage += "\n" + "Nombre de bonnes réponses: " + mGoodAnswer;
+            textMessage += "\n" + "Nombre de mauvaise réponses: " + mBadAnswer;
             builder.setTitle("Résultat")
                     .setMessage(textMessage)
                     .setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -116,7 +120,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                     })
                     .create()
                     .show();
-
         }
     }
 
@@ -125,23 +128,58 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         String responseIndex = v.getTag().toString();
         String GoodText = "";
         String BadText = "";
-        if(responseIndex == mCurrentAnswer){
+
+        this.changeColor(getResources().getColor(R.color.colorBadAnswer));
+        this.setColorGoodAnswer(mCurrentAnswer);
+
+        if (responseIndex == mCurrentAnswer) {
             mGoodAnswer++;
-            if(mGoodAnswer == 1){
+            if (mGoodAnswer == 1) {
                 GoodText = "1 bonne réponse";
-            }else{
-                GoodText = mGoodAnswer+" bonnes réponses.";
+            } else {
+                GoodText = mGoodAnswer + " bonnes réponses.";
             }
             mGoodAnswerText.setText(GoodText);
-        }else{
+        } else {
             mBadAnswer++;
-            if(mBadAnswer == 3){
+            if (mBadAnswer == 3) {
                 BadText = "1 joker";
-            }else{
-                BadText = mLevel -mBadAnswer +" jokers";
+            } else {
+                BadText = mLevel - mBadAnswer + " jokers";
             }
             mBadAnswerText.setText(BadText);
         }
-        this.displayQuestion(mlistQuestion, mGoodAnswer+mBadAnswer);
+
+        Handler timerHandler = new Handler();
+        timerHandler.postDelayed(nextQuestion, 1000);
     }
+
+    public void setColorGoodAnswer(String goodAnswer) {
+        if (mAnswer1Button.getText().toString() == goodAnswer) {
+            mAnswer1Button.setBackgroundColor(getResources().getColor(R.color.colorGoodAnswer));
+        }
+        if (mAnswer2Button.getText().toString() == goodAnswer) {
+            mAnswer2Button.setBackgroundColor(getResources().getColor(R.color.colorGoodAnswer));
+        }
+        if (mAnswer3Button.getText().toString() == goodAnswer) {
+            mAnswer3Button.setBackgroundColor(getResources().getColor(R.color.colorGoodAnswer));
+        }
+        if (mAnswer4Button.getText().toString() == goodAnswer) {
+            mAnswer4Button.setBackgroundColor(getResources().getColor(R.color.colorGoodAnswer));
+        }
+    }
+
+    public void changeColor(int color) {
+        mAnswer1Button.setBackgroundColor(color);
+        mAnswer2Button.setBackgroundColor(color);
+        mAnswer3Button.setBackgroundColor(color);
+        mAnswer4Button.setBackgroundColor(color);
+    }
+
+    private Runnable nextQuestion = new Runnable() {
+        @Override
+        public void run() {
+            displayQuestion(mlistQuestion, mGoodAnswer + mBadAnswer);
+        }
+    };
 }
